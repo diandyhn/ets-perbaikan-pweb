7774c29f33186b24c3a2cf452edb54014609bea0
Dian Kusumawati <128298460+diandyhn@users.noreply.github.com>
2023-11-06 14:48:14 +0700

    optimalisasi

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-
 $(document).ready(function(){
     var url = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0";
     $.get(url, function(data, status){
         var pokemonList = $("#pokemon-list");
 
-        for (var i = 0; i < data.results.length; i++) {
-            var name = data.results[i].name;
-            var url = data.results[i].url;
+        for (var i = 0; i < 20; i+=4) {
+            var cardRow = $('<div class="row justify-content-center" style="padding-top: 40px;"></div>');
 
-            var card = `
-                <div style="width: 400px; height: 200px;">
-                    <div class="card border-left-success shadow h-100 py-2">
+            for (var j = i; j < i+4; j++) {
+                var card = `
+                <div class="col-xl-3 col-md-6 mb-4 padding">
+                    <div class="card border-dark text-center">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
-                                <div class="col-auto">
-                                    <div class="h9 mb-0 font-weight-bold text-blue-500">${name}</div>
+                                <div class="col">
+                                    <div class="h9 mb-0 font-weight-bold text-blue-500">${data.results[j].name}</div>
                                     <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                        <button href="${url}" target="_blank">Detail</button>
+                                        <a href="${data.results[j].url}" target="_blank" class="btn btn-primary">Detail</a>
                                     </div>
-                                </div>
+                                </div>              
                             </div>
                         </div>
                     </div>
                 </div>
-            `;
+                `;
+                cardRow.append(card);
+            }
 
-            pokemonList.append(card);
+            pokemonList.append(cardRow);
         }
     });
 });
